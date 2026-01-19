8a628875260d39e45395706b60d7ee5561ea91e0
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-01-19 21:11:30 +0530

    Contains Duplicate

---
+++ DSA/Duplicate/DuplicateCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Duplicate
+{
+    public class DuplicateCheck
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if (nums==null || nums.Length < 2) return false;
+
+            HashSet<int> table = new HashSet<int>(nums.Length);
+            foreach(int i in nums)
+            {
+                if (!table.Add(i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DSA.Arrays;
+using DSA.Duplicate;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +10,12 @@ namespace DSA
     {
         public static void Main(string[] args)
         {
-            int x = int.Parse(Console.ReadLine());
-            Solution sln = new Solution();
-            Console.WriteLine(sln.IsPalindrome(x));
+            //int x = int.Parse(Console.ReadLine());
+            //Solution sln = new Solution();
+            //Console.WriteLine(sln.IsPalindrome(x));
+
+            DuplicateCheck dup = new DuplicateCheck();
+            Console.WriteLine(dup.ContainsDuplicate([1, 2, 3, 4]));
         }
     }
 }
