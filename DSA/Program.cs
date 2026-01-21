f4559a9e237eb561e4cb4398d2a93598ad46ca38
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-01-21 21:26:55 +0530

    commented

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -19,8 +19,8 @@ namespace DSA
             //Third_Maximum_Number tmn = new Third_Maximum_Number();
             //Console.WriteLine(tmn.ThirdMax([3,2,1]).ToString());
 
-            Reverse_Integer rev = new Reverse_Integer();
-            Console.WriteLine(rev.Reverse(-123).ToString());
+            // Reverse_Integer rev = new Reverse_Integer();
+            // Console.WriteLine(rev.Reverse(-123).ToString());
         }
     }
 }
