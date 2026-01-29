ecb09b7a7d38b0bfd9a266a79c640806309581e2
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-01-29 19:56:31 +0530

    Duplicate Number Improved Performance

--- DSA/Duplicate_Number.cs
+++ DSA/Duplicate_Number.cs
@@ -8,17 +8,26 @@ namespace DSA
     {
         public int FindDuplicate(int[] nums)
         {
-            int[] res = new int[nums.Length+1];
-            foreach(int i in nums)
+            //if (nums.Length < 3) return nums[0];
+
+            //bool[] res = new bool[nums.Length + 1];
+            //foreach (int i in nums)
+            //{
+            //    if (res[i] == false)
+            //        res[i] = true;
+            //    else
+            //        return i;
+            //}
+            //return -1;
+
+            if (nums.Length < 3) return nums[0];
+
+            bool[] res = new bool[nums.Length + 1];
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (res[i] == 0)
-                {
-                    res[i] = 1;
-                }
-                else
-                {
-                    return i;
-                }
+                int j = nums[i];
+                if (res[j]) return j;
+                res[j] = true;
             }
             return -1;
         }
