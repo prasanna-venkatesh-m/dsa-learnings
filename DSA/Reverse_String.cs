81a566d8443a4b40e160d5e6b4189e12c4c6fb04
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-02-06 16:42:33 +0530

    Reverse string optimal approach

---
+++ DSA/Reverse_String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Reverse_String
+    {
+        public void ReverseString(char[] s)
+        {
+            int start = 0;
+            int end = s.Length - 1;
+
+            while(start < end)
+            {
+                char temp = s[start];
+                s[start++] = s[end];
+                s[end--] = temp;
+            }
+        }
+    }
+}
