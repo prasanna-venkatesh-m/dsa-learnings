f1bff446026c9f79365cbffffda855385c821ab1
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-02-08 19:31:15 +0530

    Add Strings

---
+++ DSA/Add_Strings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace DSA
+{
+    public class Add_Strings
+    {
+        public string AddStrings(string num1, string num2)
+        {
+            BigInteger a = BigInteger.Parse(num1);
+            BigInteger b = BigInteger.Parse(num2);
+            return a + b + "";
+        }
+    }
+}
