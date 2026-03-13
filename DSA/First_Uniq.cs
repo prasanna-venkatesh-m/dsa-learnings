98dbcb30276e6bf3a3005c8ae64d0bd644fb6380
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-13 21:23:25 +0530

    First Unique character brute force approach

---
+++ DSA/First_Uniq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class First_Uniq
+    {
+        public int FirstUniqChar(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[j] == s[i] && i != j)
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
