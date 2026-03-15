da66590bd2ac140be3f702512d522bc0ec8ee79d
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-15 19:32:27 +0530

    Feat : Is Perfect square

---
+++ DSA/IsPerfect_Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class IsPerfect_Square
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            int start = 1;
+            int end = num;
+
+            while (start <= end)
+            {
+                long mid = start + (end - start) / 2;
+                long square = mid * mid;
+
+                if (square == num)
+                {
+                    return true;
+                }
+                else if (square > num)
+                {
+                    end = (int)mid - 1;
+                }
+                else
+                {
+                    start = (int)mid + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
