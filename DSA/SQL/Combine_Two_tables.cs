22e6c3b4c7c614cb6a520ea2e1d7856032fecbd3
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-16 21:29:19 +0530

    Coimbing two table optimized code

--- DSA/SQL/Combine_Two_tables.cs
+++ DSA/SQL/Combine_Two_tables.cs
@@ -12,43 +12,42 @@ namespace DSA.SQL
     internal class Combine_Two_tables
     {
 
-        //        Write a solution to report the first name, last name, city, and state of each person in the Person table.If the address of a personId is not present in the Address table, report null instead.
-        //Return the result table in any order.
-        //The result format is in the following example.
+            //        Write a solution to report the first name, last name, city, and state of each person in the Person table.If the address of a personId is not present in the Address table, report null instead.
+            //Return the result table in any order.
+            //The result format is in the following example.
 
-        //Example 1:
+            //Example 1:
 
-        //Input: 
-        //Person table:
-        //+----------+----------+-----------+
-        //| personId | lastName | firstName |
-        //+----------+----------+-----------+
-        //| 1        | Wang     | Allen     |
-        //| 2        | Alice    | Bob       |
-        //+----------+----------+-----------+
-        //Address table:
-        //+-----------+----------+---------------+------------+
-        //| addressId | personId | city          | state      |
-        //+-----------+----------+---------------+------------+
-        //| 1         | 2        | New York City | New York   |
-        //| 2         | 3        | Leetcode      | California |
-        //+-----------+----------+---------------+------------+
-        //Output: 
-        //+-----------+----------+---------------+----------+
-        //| firstName | lastName | city          | state    |
-        //+-----------+----------+---------------+----------+
-        //| Allen     | Wang     | Null          | Null     |
-        //| Bob       | Alice    | New York City | New York |
-        //+-----------+----------+---------------+----------+
-        //Explanation: 
-        //There is no address in the address table for the personId = 1 so we return null in their city and state.
-        //addressId = 1 contains information about the address of personId = 2.
+            //Input: 
+            //Person table:
+            //+----------+----------+-----------+
+            //| personId | lastName | firstName |
+            //+----------+----------+-----------+
+            //| 1        | Wang     | Allen     |
+            //| 2        | Alice    | Bob       |
+            //+----------+----------+-----------+
+            //Address table:
+            //+-----------+----------+---------------+------------+
+            //| addressId | personId | city          | state      |
+            //+-----------+----------+---------------+------------+
+            //| 1         | 2        | New York City | New York   |
+            //| 2         | 3        | Leetcode      | California |
+            //+-----------+----------+---------------+------------+
+            //Output: 
+            //+-----------+----------+---------------+----------+
+            //| firstName | lastName | city          | state    |
+            //+-----------+----------+---------------+----------+
+            //| Allen     | Wang     | Null          | Null     |
+            //| Bob       | Alice    | New York City | New York |
+            //+-----------+----------+---------------+----------+
+            //Explanation: 
+            //There is no address in the address table for the personId = 1 so we return null in their city and state.
+            //addressId = 1 contains information about the address of personId = 2.
 
 
         /* Write your T-SQL query statement below */
-        //SELECT firstName, lastName, city, state
-        //FROM Person
-        //LEFT JOIN Address
-        //ON Person.personId = Address.personId;
+        //SELECT p.firstName, p.lastName, a.city, a.state
+        //FROM Person p
+        //LEFT JOIN Address a on p.personId = a.personId
     }
 }
