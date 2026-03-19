f495a9b7d52f03f98de4aff46a75578f071f1879
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-19 18:20:38 +0530

    Is Power of Two optimal solution

---
+++ DSA/Power_of_two.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Power_of_two
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 0) return false;
+            double power = Math.Round(Math.Log(n) / Math.Log(2));
+            double res = Math.Pow(2, power);
+            return res == n;
+        }
+    }
+}
