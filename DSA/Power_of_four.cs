d9b1a4130e185ad09b2ceb5c644eeb967ac24e33
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-19 18:22:05 +0530

    Power of four optimal solution

---
+++ DSA/Power_of_four.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Power_of_four
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            if (n == 0) return false;
+            double power = Math.Round(Math.Log(n) / Math.Log(4));
+            double res = Math.Pow(4, power);
+            return res == n;
+        }
+    }
+}
