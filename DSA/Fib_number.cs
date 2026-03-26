a60cf44cded6621e865311a1cdfcbb48027cf27f
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-26 21:50:43 +0530

    Fib number

---
+++ DSA/Fib_number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Fib_number
+    {
+        public int Fib(int n)
+        {
+            if(n== 0) return 0;
+            if(n== 1) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
