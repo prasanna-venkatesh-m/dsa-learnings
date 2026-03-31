475232337f9bd7614ebec9ec3e05380e357e93b1
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-03-31 20:34:42 +0530

    FizzBuzz better approach

--- DSA/FizzBuss.cs
+++ DSA/FizzBuss.cs
@@ -11,22 +11,12 @@ namespace DSA
             List<string> answer = new List<string>();
             for(int i = 1; i <= n; i++)
             {
-                if((i % 3 == 0) && (i % 5 == 0))
-                {
-                    answer.Add("FizzBuzz");
-                }
-                else if (i % 5 == 0)
-                {
-                    answer.Add("Buzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    answer.Add("Fizz");
-                }
-                else
-                {
-                    answer.Add(i.ToString());
-                }
+                string res = string.Empty;
+                if (i % 3 == 0) res += "Fizz";
+                if (i % 5 == 0) res += "Buzz";
+
+                if (!string.IsNullOrEmpty(res)) answer.Add(res);
+                else answer.Add(i.ToString());
             }
             return answer;
         }
