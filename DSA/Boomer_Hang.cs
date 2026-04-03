107fdfffa9178b1526a59e92ec6a6f0e898010d8
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-04-03 21:56:08 +0530

    Boomerang optimal solution

---
+++ DSA/Boomer_Hang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Boomer_Hang
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            return (points[0][0] * (points[1][1] - points[2][1]))
+                + (points[1][0] * (points[2][1] - points[0][1]))
+                + (points[2][0] * (points[0][1] - points[1][1]))
+                != 0;
+        }
+    }
+}
