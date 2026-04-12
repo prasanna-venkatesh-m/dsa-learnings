2393c149ba3fe2adbb4125be931bdca1dfba89b9
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-04-12 18:37:18 +0530

    First Matching index solution

---
+++ DSA/First_Matching_Index.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class First_Matching_Index
+    {
+        public int FirstMatchingIndex(string s)
+        {
+            int start = 0;
+            int end = s.Length - 1;
+            while(start <= end)
+            {
+                if (s[start] == s[end])
+                {
+                    return start;
+                }
+                start++;
+                end--;
+            }
+            return -1;
+        }
+    }
+}
