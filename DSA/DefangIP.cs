abeeecef56b753ef9bdb8c6a0b77542712f4cfd0
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-04-13 17:13:18 +0530

    DefangIP Address Brute force approach

---
+++ DSA/DefangIP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class DefangIP
+    {
+        public string DefangIPaddr(string address)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in address)
+            {
+                if(c == '.')
+                {
+                    sb.Append("[.]");
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
