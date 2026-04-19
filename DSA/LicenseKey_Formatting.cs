b3ae203bf4a91fcba132b7aa7218f2d21c867a63
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-04-19 20:28:38 +0530

    License Key formatting brute force solution

---
+++ DSA/LicenseKey_Formatting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DSA
+{
+    public class LicenseKey_Formatting
+    {
+        public string LicenseKeyFormatting(string s, int k)
+        {
+            string new_s = s.Replace("-", "").ToUpper();
+            int i = new_s.Length;
+            while(i >= 0)
+            {
+                i = i - k;
+                if(i > 0)
+                {
+                    new_s = new_s.Insert(i, "-");
+                }
+            }
+            return new_s;
+        }
+    }
+}
