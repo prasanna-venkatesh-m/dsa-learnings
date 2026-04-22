4bb0ec5a1cd117d84926bc8b1cc862e0f5ca7fd9
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-04-22 19:32:16 +0530

    License Key Formatting different approach

--- DSA/LicenseKey_Formatting.cs
+++ DSA/LicenseKey_Formatting.cs
@@ -9,25 +9,25 @@ namespace DSA
     {
         public string LicenseKeyFormatting(string s, int k)
         {
-            StringBuilder sb = new StringBuilder();
+            String sb = "";
             int reset = 0;
             int i = s.Length - 1;
             while (i >= 0)
             {
                 if (reset == k && s[i]!='-') {
-                    sb.Insert(0, '-');
+                    sb.Insert(0, "-");
                     reset = 0;
                 }
 
                 if (s[i] == '-') i--;
                 else
                 {
-                    sb.Insert(0, char.ToUpper(s[i]));
+                    sb.Insert(0, char.ToUpper(s[i])+"");
                     i--;
                     reset++;
                 }
             }
-            return sb.ToString();
+            return sb;
         }
     }
 }
