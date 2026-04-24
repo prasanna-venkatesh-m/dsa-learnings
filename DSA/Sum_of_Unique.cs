e5056471b085956e300d8c949742eb079eb983ba
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-04-24 17:41:11 +0530

    Sum of Unique Brute Force solution

---
+++ DSA/Sum_of_Unique.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Sum_of_Unique
+    {
+        public int SumOfUnique(int[] nums)
+        {
+            Hashtable ht = new Hashtable();
+            for(int i=0; i<nums.Length; i++)
+            {
+                if (ht.ContainsKey(nums[i]))
+                {
+                    ht[nums[i]] = 1 + (int)ht[nums[i]];
+                }
+                else
+                {
+                    ht.Add(nums[i], 1);
+                }
+            }
+            
+            int res = 0;
+            foreach (DictionaryEntry entry in ht)
+            {
+                if ((int)entry.Value == 1)
+                {
+                    res += (int)entry.Key;
+                }
+            }
+            return res;
+        }
+    }
+}
