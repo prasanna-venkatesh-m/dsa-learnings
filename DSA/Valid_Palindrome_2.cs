0bc0c1f9b84786cfbe1e827aed5fc43736798047
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-05-11 13:45:06 +0530

    Valid Palindrome 2 Brute force approach

---
+++ DSA/Valid_Palindrome_2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Valid_Palindrome_2
+    {
+        public bool ValidPalindrome(string s)
+        {
+            for(int i=0; i< s.Length; i++)
+            {
+                string temp = s.Substring(0, i) + s.Substring(i+1);
+                if (checkPalindrome(temp)) return true;
+            }
+            return false;
+        }
+
+        public bool checkPalindrome(string word)
+        {
+            int i = 0;
+            int j = word.Length - 1;
+            while(i < j)
+            {
+                if (word[i] == word[j])
+                {
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
