50c61539add794fb53a3f91ded1ac659c350e316
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-08 17:33:56 +0530

    SQL : Rising Temperature Different solution

--- DSA/SQL/Rising_Temperature.cs
+++ DSA/SQL/Rising_Temperature.cs
@@ -61,5 +61,18 @@ namespace DSA.SQL
         //id
         //FROM Weather w
         //WHERE w.temperature  > (SELECT temperature  FROM Weather WHERE recordDate= DATEADD(DAY, -1, w.recordDate))
+
+        //        SELECT id
+        //FROM(
+        //    SELECT
+        //        id,
+        //        recordDate,
+        //        temperature,
+        //        LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp,
+        //        LAG(recordDate) OVER(ORDER BY recordDate) AS prev_date
+        //    FROM Weather
+        //) w
+        //WHERE DATEDIFF(DAY, prev_date, recordDate) = 1
+        //  AND temperature > prev_temp;
     }
 }
