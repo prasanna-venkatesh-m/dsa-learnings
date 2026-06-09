fde29fd1c08d31dd563a3cc7f11171fc23543233
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-09 16:27:22 +0530

    Merge Sorted Linked List

---
+++ DSA/Merge_Sorted_Linked_List.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Merge_Sorted_Linked_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode tail = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    tail.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    tail.next = list2;
+                    list2 = list2.next;
+                }
+
+                tail = tail.next;
+            }
+
+            tail.next = list1 ?? list2;
+
+            return dummy.next;
+        }
+    }
+}
