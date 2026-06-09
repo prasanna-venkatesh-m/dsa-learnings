260e4a08a2fe3e46a16733f413dd514297a52f76
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-09 17:21:16 +0530

    SQL : User Activity Past 30 days Optimal solution

--- DSA/SQL/User_Activity_Past30_days_I.cs
+++ DSA/SQL/User_Activity_Past30_days_I.cs
@@ -74,5 +74,12 @@ namespace DSA.SQL
         //FROM Activity
         //WHERE activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
         //GROUP BY activity_date
+
+        //SELECT
+        //activity_date AS day,
+        //COUNT(DISTINCT(user_id)) AS active_users
+        //FROM Activity
+        //GROUP BY activity_date
+        //HAVING activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
     }
 }
