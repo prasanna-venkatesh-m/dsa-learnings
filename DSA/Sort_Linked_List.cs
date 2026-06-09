08c3ed828a6a8b857568f62f994c903eeb3cb0da
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-09 17:00:13 +0530

    Sort Linked List Brute force approach

---
+++ DSA/Sort_Linked_List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static DSA.Merge_Sorted_Linked_List;
+
+namespace DSA
+{
+    public class Sort_Linked_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode SortList(ListNode head)
+        {
+            List<int> values = new List<int>();
+            ListNode temp = head;
+            while (temp != null)
+            {
+                values.Add(temp.val);
+                temp = temp.next;
+            }
+
+            values.Sort();
+
+            ListNode res = head;
+            int i = 0;
+            while (res != null)
+            {
+                res.val = values[i++];
+
+                res = res.next;
+            }
+            return head;
+        }
+    }
+}
