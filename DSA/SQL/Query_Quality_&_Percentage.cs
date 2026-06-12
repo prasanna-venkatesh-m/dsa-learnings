8541c90dcb5c46be70f8740a332e6d6aca3f6e9b
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-12 13:53:33 +0530

    SQL : Query Quality and Percentage Optimal solution

--- DSA/SQL/Query_Quality_&_Percentage.cs
+++ DSA/SQL/Query_Quality_&_Percentage.cs
@@ -6,7 +6,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
 using System.Reflection.Emit;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
+using System.Text.RegularExpressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DSA.SQL
@@ -92,5 +94,12 @@ namespace DSA.SQL
         //FROM
         //Queries) as fil
         //GROUP BY fil.query_name
+
+        //        SELECT
+        //    query_name,
+        //    ROUND(AVG(rating* 1.0 / position), 2) AS quality,
+        //    ROUND(100.0 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*), 2) AS poor_query_percentage
+        //FROM Queries
+        //GROUP BY query_name;
     }
 }
