93eb9f03ef4b25f8696368e211def3a9cadb827b
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-15 13:40:56 +0530

    SQL : Average Selling price

---
+++ DSA/SQL/Average_Selling_Price.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Text;
+using System.Xml;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DSA.SQL
+{
+    internal class Average_Selling_Price
+    {
+        //        Table: Prices
+
+        //+---------------+---------+
+        //| Column Name   | Type    |
+        //+---------------+---------+
+        //| product_id    | int     |
+        //| start_date    | date    |
+        //| end_date      | date    |
+        //| price         | int     |
+        //+---------------+---------+
+        //(product_id, start_date, end_date) is the primary key(combination of columns with unique values) for this table.
+        //Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+        //For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+
+
+        //Table: UnitsSold
+
+        //+---------------+---------+
+        //| Column Name   | Type    |
+        //+---------------+---------+
+        //| product_id    | int     |
+        //| purchase_date | date    |
+        //| units         | int     |
+        //+---------------+---------+
+        //This table may contain duplicate rows.
+        //Each row of this table indicates the date, units, and product_id of each product sold. 
+
+
+
+        //Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
+
+        //Return the result table in any order.
+
+        //The result format is in the following example.
+
+
+
+        //Example 1:
+
+        //Input: 
+        //Prices table:
+        //+------------+------------+------------+--------+
+        //| product_id | start_date | end_date   | price  |
+        //+------------+------------+------------+--------+
+        //| 1          | 2019-02-17 | 2019-02-28 | 5      |
+        //| 1          | 2019-03-01 | 2019-03-22 | 20     |
+        //| 2          | 2019-02-01 | 2019-02-20 | 15     |
+        //| 2          | 2019-02-21 | 2019-03-31 | 30     |
+        //+------------+------------+------------+--------+
+        //UnitsSold table:
+        //+------------+---------------+-------+
+        //| product_id | purchase_date | units |
+        //+------------+---------------+-------+
+        //| 1          | 2019-02-25    | 100   |
+        //| 1          | 2019-03-01    | 15    |
+        //| 2          | 2019-02-10    | 200   |
+        //| 2          | 2019-03-22    | 30    |
+        //+------------+---------------+-------+
+        //Output: 
+        //+------------+---------------+
+        //| product_id | average_price |
+        //+------------+---------------+
+        //| 1          | 6.96          |
+        //| 2          | 16.96         |
+        //+------------+---------------+
+        //Explanation: 
+        //Average selling price = Total Price of Product / Number of products sold.
+        //Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
+        //Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
+
+        //        SELECT
+        //fil.product_id,
+        //CAST(CAST(SUM(fil.pu) as DECIMAL(10,2)) / SUM(fil.units) as DECIMAL(10,2)) as average_price
+        //FROM(
+        //    SELECT
+        //p.product_id,
+        //p.price,
+        //u.units,
+        //(p.price* u.units) as pu
+        //FROM
+        //Prices p
+        //JOIN UnitsSold u on p.product_id = u.product_id AND(u.purchase_date >= p.start_date) AND(u.purchase_date <= p.end_date)
+        //) as fil
+        //GROUP BY fil.product_id
+    }
+}
