decab309117cf6875a82eedf0a1cfa0adb619d11
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-17 13:28:13 +0530

    SQL : Group Sold Product by Date

---
+++ DSA/SQL/Group_Sold_Product_By_Date.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Globalization;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DSA.SQL
+{
+    internal class Group_Sold_Product_By_Date
+    {
+
+        //Table Activities:
+
+        //+-------------+---------+
+        //| Column Name | Type    |
+        //+-------------+---------+
+        //| sell_date   | date    |
+        //| product     | varchar |
+        //+-------------+---------+
+        //There is no primary key(column with unique values) for this table.It may contain duplicates.
+        //Each row of this table contains the product name and the date it was sold in a market.
+
+
+
+        //Write a solution to find for each date the number of different products sold and their names.
+
+        //The sold products names for each date should be sorted lexicographically.
+
+        //Return the result table ordered by sell_date.
+
+        //The result format is in the following example.
+
+
+
+        //Example 1:
+
+        //Input: 
+        //Activities table:
+        //+------------+------------+
+        //| sell_date  | product     |
+        //+------------+------------+
+        //| 2020-05-30 | Headphone  |
+        //| 2020-06-01 | Pencil     |
+        //| 2020-06-02 | Mask       |
+        //| 2020-05-30 | Basketball |
+        //| 2020-06-01 | Bible      |
+        //| 2020-06-02 | Mask       |
+        //| 2020-05-30 | T-Shirt    |
+        //+------------+------------+
+        //Output: 
+        //+------------+----------+------------------------------+
+        //| sell_date  | num_sold | products                     |
+        //+------------+----------+------------------------------+
+        //| 2020-05-30 | 3        | Basketball, Headphone, T-shirt |
+        //| 2020-06-01 | 2        | Bible, Pencil                 |
+        //| 2020-06-02 | 1        | Mask                         |
+        //+------------+----------+------------------------------+
+        //Explanation: 
+        //For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them lexicographically and separate them by a comma.
+        //For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
+        //For 2020-06-02, the Sold item is (Mask), we just return it.
+
+        //        SELECT
+        //e.sell_date,
+        //COUNT(e.product) as num_sold,
+        //STRING_AGG(product, ',') as products
+        //FROM(
+        //    SELECT DISTINCT product, sell_date
+        //    FROM Activities
+        //) e
+        //GROUP BY e.sell_date
+
+    }
+}
