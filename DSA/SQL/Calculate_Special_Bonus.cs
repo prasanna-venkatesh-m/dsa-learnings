d6c7e21b3aaa9f3deacdf8142d29b1edd3fca3f4
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-06-27 19:11:53 +0530

    SQL : Calculate Special Bonus Order by included

--- DSA/SQL/Calculate_Special_Bonus.cs
+++ DSA/SQL/Calculate_Special_Bonus.cs
@@ -67,5 +67,6 @@ namespace DSA.SQL
         //employee_id,
         //CASE WHEN((employee_id % 2)=1 AND(name NOT LIKE 'M%')) THEN salary ELSE 0 END as bonus
         //FROM Employees
+        //ORDER BY employee_id
     }
     }
