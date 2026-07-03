531e593107f82971e11e2542bfa8c853aa04b64c
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-07-03 19:00:50 +0530

    SQL : Employees with Missing information

---
+++ DSA/SQL/Employees_With_Missing_Information.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace DSA.SQL
+{
+    internal class Employees_With_Missing_Information
+    {
+        //Table: Employees
+
+        //+-------------+---------+
+        //| Column Name | Type    |
+        //+-------------+---------+
+        //| employee_id | int     |
+        //| name        | varchar |
+        //+-------------+---------+
+        //employee_id is the column with unique values for this table.
+        //Each row of this table indicates the name of the employee whose ID is employee_id.
+
+
+        //Table: Salaries
+
+        //+-------------+---------+
+        //| Column Name | Type    |
+        //+-------------+---------+
+        //| employee_id | int     |
+        //| salary      | int     |
+        //+-------------+---------+
+        //employee_id is the column with unique values for this table.
+        //Each row of this table indicates the salary of the employee whose ID is employee_id.
+
+        //SELECT
+        //e.employee_id FROM
+        //Employees e
+        //LEFT JOIN Salaries s on e.employee_id = s.employee_id
+        //WHERE s.employee_id IS NULL
+
+        //UNION
+
+        //SELECT
+        //s.employee_id FROM
+        //Salaries s
+        //LEFT JOIN Employees e on s.employee_id = e.employee_id
+        //WHERE e.employee_id IS NULL
+    }
+    }
