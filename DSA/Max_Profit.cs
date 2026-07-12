1954348cc501714defe6c070f1f274a14e66a3bf
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-07-12 19:39:43 +0530

    Max Profit Brute force solution

---
+++ DSA/Max_Profit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSA
+{
+    public class Max_Profit
+    {
+        public int MaxProfit(int[] prices)
+        {
+            List<int> res = new List<int>();
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    res.Add(prices[j] - prices[i]);
+                }
+            }
+            return res.Count() > 0 ? (res.Max() > 0 ? res.Max() : 0)  : 0;
+        }
+    }
+}
