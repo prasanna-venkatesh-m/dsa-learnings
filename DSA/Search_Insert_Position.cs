a9d50e8dff3ee66f3c5b45f0f52a7c0936c7b362
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-07-12 18:27:29 +0530

    Search Insert Position optimal solution

---
+++ DSA/Search_Insert_Position.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DSA
+{
+    public class Search_Insert_Position
+    {
+        //Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+        //You must write an algorithm with O(log n) runtime complexity.
+
+        //Example 1:
+
+        //Input: nums = [1, 3, 5, 6], target = 5
+        //Output: 2
+        //Example 2:
+
+        //Input: nums = [1, 3, 5, 6], target = 2
+        //Output: 1
+        //Example 3:
+
+        //Input: nums = [1, 3, 5, 6], target = 7
+        //Output: 4
+        //Constraints:
+        //1 <= nums.length <= 104
+        //-104 <= nums[i] <= 104
+        //nums contains distinct values sorted in ascending order.
+        //-104 <= target <= 104
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int i = 0;
+            while (i < nums.Length && nums[i] <= target)
+            {
+                if (nums[i] == target) return i;
+                else i++;
+            }
+            return i;
+        }
+    }
+}
