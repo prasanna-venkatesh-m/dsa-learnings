b664ce524aa8f320849ad41316e4581dc4f7e1e2
prasanna-venkatesh-m <m.prasannavenkatesh9@gmail.com>
2026-07-23 17:45:03 +0530

    Excel Title to Number solution

---
+++ DSA/TitleToNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class TitleToNumbers
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            double res = 0;
+            int length = columnTitle.Length;
+            for (int i = length-1; i >= 0; i--)
+            {
+                res += (Math.Pow(26, length - i-1)) + (columnTitle[i] - 65);
+            }
+            return (int)res;
+        }
+    }
+}
